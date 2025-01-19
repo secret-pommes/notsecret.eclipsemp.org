686a37b4f5c7f19224f9606b93d4d4d72ae4762a
Secret1337 <97087168+secret-pommes@users.noreply.github.com>
2025-01-19 22:31:45 +0100

    profi

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import engine from "./assets/engine.mp4";
+import wallace from "./assets/wallace.gif";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("not_secret1337");
+  const [isAnimating, setIsAnimating] = useState(false);
+  const speed = 250;
+
+  const handleMouseEnter = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setName("secret pommes");
+      setIsAnimating(false);
+    }, speed);
+  };
+
+  const handleMouseLeave = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setName("not_secret1337");
+      setIsAnimating(false);
+    }, speed);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <video className="bg-video" autoPlay muted loop>
+        <source src={engine} type="video/mp4" />
+      </video>
+      <center>
+        <div className="ContainerThing">
+          <div className="Profile">
+            <h1
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={`name ${isAnimating ? "hide" : ""}`}
+            >
+              {name}
+            </h1>
+            <div className="pfp-container">
+              <i
+                className="fab fa-discord icon discord"
+                onClick={() =>
+                  window.open("https://discord.gg/eclipsemp", "_blank")
+                }
+              />
+              <img src={wallace} alt="pfp" className="pfp" />
+              <i
+                className="fab fa-github icon github"
+                onClick={() =>
+                  window.open("https://github.com/secret-pommes", "_blank")
+                }
+              />
+            </div>
+            <p className="desc">
+              I do Backend most, and i enjoy TypeScript.
+              <br />I also work on{" "}
+              <a href="https://discord.gg/eclipsemp" className="eclipsewtf">
+                Eclipse
+              </a>{" "}
+              a private server for Season 3.
+            </p>
+          </div>
+        </div>
+      </center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
